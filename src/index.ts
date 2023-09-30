6bf688a6081bd5906aac70b357310154e024fb8d
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-09-30 20:52:21 +0100

    update inde.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 import express from 'express';
+
+const server = express()
+
+server.get('/', (req, res) =>{
+    res.send('Hello, World')
+});
+
+server.listen(3333, () =>{
+    console.log('Server running in 3333')
+})
