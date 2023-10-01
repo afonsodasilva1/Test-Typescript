6a0c9b361a5d08d3b945cb598350ffb744452b97
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-10-01 04:01:32 +0100

    feat: Add eslint prettier

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import express from 'express';
 const server = express();
 
 server.get('/', (_req, res) =>{
-    res.send('Hello, World');
+  res.send('Hello, World');
 });
 
+
 server.listen(3333, () => {
-    console.log('Server running in 3333');
+  console.log('Server running in 3333');
 });
