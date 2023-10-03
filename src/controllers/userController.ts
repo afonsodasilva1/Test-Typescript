d5889cdbc2160f16dd3f995892761ccc682aa9a3
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-10-03 20:41:57 +0100

    v1

---
+++ src/controllers/userController.ts
@@ -0,0 +1,13 @@
+import { Response, Request } from "express";
+
+interface IUser {
+    name: string,
+    email: string,
+    id: string
+    password: string
+}
+const userMemory: IUser[] = []
+
+export const userIndex = async (req: Request, res: Response)  =>{
+
+}
